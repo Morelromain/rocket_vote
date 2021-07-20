36ffc3410c2ea42f6ae6a0f6e6df06b13369497c
Romain Morel <morelromain86@gmail.com>
2021-07-20 11:12:40 +0200

    finalise for mobile

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY'] # Prod
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False # Prod
+DEBUG = True # Prod
 #DEBUG = True # Local
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', "localhost"]
@@ -118,11 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-
 #STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = 'static'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
